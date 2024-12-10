2d14fe4e07683f8bd3cd78215e2ee4378cf9f580
Ihor Novykov <ihor.n@develops.today>
2024-12-10 20:27:42 +0200

    feat: implemented patient page & made permissions on the patients page

--- src/app/dashboard/patients/components/cell-action.tsx
+++ src/app/dashboard/patients/components/cell-action.tsx
@@ -1,30 +1,7 @@
 'use client';
-import { AlertModal } from '@/components/modal/alert-modal';
-import { Button, buttonVariants } from '@/components/ui/button';
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu';
-import {
-  Edit,
-  MoreHorizontal,
-  NotebookPen,
-  SquareArrowOutUpRight,
-  Trash,
-} from 'lucide-react';
-import { useState } from 'react';
-import { UpdateUserModal } from '@/app/dashboard/employees/components/update-user-modal';
-import { ModifiedEmployee } from '@/types/employee';
-import { api } from '@/lib/trpc/client';
-import useEmployeesStore from '@/hooks/store/use-employees-store';
-import { useToast } from '@/hooks/use-toast';
-import { Service } from '@/types/service';
-import ManageServiceModal from '@/app/dashboard/services/components/manage-service-modal';
-import useServicesStore from '@/hooks/store/use-services-store';
+import { buttonVariants } from '@/components/ui/button';
+import { NotebookPen, SquareArrowOutUpRight } from 'lucide-react';
 import Link from 'next/link';
-import { cva } from 'class-variance-authority';
 import { cn } from '@/utils/cn';
 import { Patient } from '@/types/patient';
 import { useSession } from 'next-auth/react';
